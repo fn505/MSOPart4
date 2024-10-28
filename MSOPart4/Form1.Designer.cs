6d309dbf253f09d0c9d892af29dec43e9e9f7dc5
0532843 <n.farah@students.uu.nl>
2024-10-28 12:25:17 +0100

    previous commit

--- MSOPart4/Form1.Designer.cs
+++ MSOPart4/Form1.Designer.cs
@@ -37,8 +37,6 @@
             ClientSize = new Size(800, 450);
             Name = "Form1";
             Text = "Form1";
-            Load += Form1_Load;
-            Click += Form1_Click;
             ResumeLayout(false);
         }
 
